890daeed3f44e155748e3b43258faab8d306d933
eloyms262 <eloyMs26@gmail.com>
2021-07-25 11:54:12 +0800

    "Added form validation and user stock relation"

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,5 +7,24 @@ has_many :stocks, through: :user_stocks
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
+    def already_track?(ticker_symbol)
+      stock = Stock.check_db(ticker_symbol)
+      return false unless stock
+        stocks.where(id: stock.id).exists?
+    end
+
+    def under_stock_limit?
+      stocks.count < 10
+    end
+
+    def can_track_stock?(ticker_symbol)
+      under_stock_limit? && !already_track?(ticker_symbol)
+      
+    end
+
+    def full_name
+    return "#{first_name} #{last_name}" if first_name || last_name
+      "Anonymous"
+    end
   
 end
